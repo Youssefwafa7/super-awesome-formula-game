9c42d8f03ec01d84e80998f5a9e9da33e0a91ecd
Omar Hassan <149178126+OmarHassan2003@users.noreply.github.com>
2026-05-01 19:11:00 +0300

    feat: added the code to load hdr sky images (#21)

--- source/common/texture/texture-utils.hpp
+++ source/common/texture/texture-utils.hpp
@@ -10,6 +10,7 @@ namespace our::texture_utils {
     // This function create an empty texture with a specific format (useful for framebuffers)
     Texture2D* empty(GLenum format, glm::ivec2 size);
     Texture2D* loadImage(const std::string& filename, bool generate_mipmap = true);
+    Texture2D* loadImageHDR(const std::string& filename);
     // This function loads an image from memory and sends its data to the given Texture2D
     Texture2D* loadImageFromMemory(const unsigned char* data, int length, bool generate_mipmap = true);
 }
